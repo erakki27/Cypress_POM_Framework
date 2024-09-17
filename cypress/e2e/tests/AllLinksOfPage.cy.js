ca44edba999c1f8b2feac032fd1ccd0fa1d53a3e
erakki27 <erakki27@gmail.com>
2024-09-17 16:54:44 +0530

    git

--- cypress/e2e/tests/AllLinksOfPage.cy.js
+++ cypress/e2e/tests/AllLinksOfPage.cy.js
@@ -1,9 +1,13 @@
+import addProductpage from "../../pageObjects/addProduct"
 describe('All links of webpage',() =>{
 
     beforeEach(function (){
         cy.visit('https://www.amazon.in/')
     })
-  it('Get all links and print' ,() =>{
+
+    const addProductobj = new addProductpage();
+
+  /*it('Get all links and print' ,() =>{
     cy.get('a').each(link => {
         if(link.prop('href'))
             cy.request({
@@ -11,6 +15,16 @@ describe('All links of webpage',() =>{
         failOnStatusCode: false
         })
         cy.log( link.prop('href'))
+    })
+
+  })*/
+
+  it('Get all cart text' ,() =>{
+    addProductobj.searchbar('Mobile');
+        addProductobj.clickonsearch();
+    cy.xpath('//span[@class="a-button a-button-primary a-button-icon"]//button').each(($ele,index) => {
+      expect($ele).to.contain("Add to cart")
+        
     })
 
   })
