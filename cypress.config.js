1f530ce9582427784458648dc6e0307386bc4ee3
erakki27 <erakki27@gmail.com>
2024-09-18 11:14:45 +0530

    git1

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  experimentalInteractiveRunEvents: true,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      quite: true,
+      overwrite: false,
+      html: true,
+      json: false,
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
